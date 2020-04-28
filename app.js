b802441db12183f9a9c7279053187b977db89d02
Sanctusz <yohans89@gmail.com>
2020-04-28 13:11:36 +0000

    much implement very bulma

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
+const favicon = require("serve-favicon");
 
 app.use(bodyParser.json())
-
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon("./public/images/favicon.ico"));
 
 app.set("view-engine", "ejs");
 
