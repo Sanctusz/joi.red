30ec5b3b5dc0a3d99174fd4574a703b8bf4f2a80
Sanctusz <yohans89@gmail.com>
2020-09-02 20:37:37 +0000

    Removed testads.ejs since I am no longer testing the google ads.

--- app.js
+++ app.js
@@ -10,27 +10,15 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 app.set("view-engine", "ejs");
 
-app.get("/", function (req, res) {
-	res.render("index.ejs");
-});
-
-app.get("/contact", function (req, res) {
-	res.render("contact.ejs");
-});
-
-app.get("/testads", function (req, res) {
-	res.render("testads.ejs");
-});
-
 // Build list of skills & proficiency to populate the table in About Page
 let languages = ["C#", "C++", "Java", "Python", "Dart", "Javascript", "HTML", "CSS", "SCSS"];
-let languagesStars = [4, 3, 3, 4, 4, 4, 5, 4, 3];
+let languagesStars = [4, 3, 4, 4, 4, 4, 5, 4, 3];
 let frontDev = ["Flutter", "React"];
-let frontDevStars = [4, 3];
+let frontDevStars = [4, 4];
 let gameEngines = ["Unity", "Godot"];
 let gameEnginesStars = [4, 3];
 let webServices = [".NET Core", "Web API", "NodeJS", "ExpressJS"];
-let webServicesStars = [3, 3, 4, 4];
+let webServicesStars = [4, 4, 4, 4];
 let cloudServices = ["AWS", "DigitalOcean"];
 let cloudServicesStars = [4, 4];
 let databases = ["PostgreSQL", "MySQL", "NoSQL"];
@@ -38,13 +26,21 @@ let databasesStars = [4, 3, 4];
 let environments = ["Windows", "Linux"];
 let environmentsStars = [5, 4];
 let other = ["SEO", "CI", "Docker", "Kubernetes"];
-let otherStars = [3, 3, 4, 3];
+let otherStars = [3, 4, 4, 3];
 
 let skills = [languages, frontDev, gameEngines, webServices, cloudServices, databases, environments, other];
 let stars = [languagesStars, frontDevStars, gameEnginesStars, webServicesStars, cloudServicesStars, databasesStars, environmentsStars, otherStars];
 let categories = ["Programming Languages", "Front End Development", "Game Engines", "Web Services", "Cloud Services", "Databases", "Environments", "Other"];
 
 let tableContent = { skills, stars, categories };
+
+app.get("/", function (req, res) {
+	res.render("index.ejs");
+});
+
+app.get("/contact", function (req, res) {
+	res.render("contact.ejs");
+});
 
 app.get("/about", function (req, res) {
 	res.render("about.ejs", { tableContent: tableContent });
